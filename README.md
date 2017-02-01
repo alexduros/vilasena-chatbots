df516b7d6851a256b23cd9dc1699877516064662
Alexandre Duros <alexandre.duros@gmail.com>
2017-02-01 14:35:46 +0100

    added mail capabilities

--- README.md
+++ README.md
@@ -12,6 +12,8 @@ Hello, I'm Dudu Nobre and I help Alex organize Vila Sena training sessions.
 * `samba`: A must-have-seen concert of me.
 * `qui a répondu au doodle ?`: I will check the doodle to show who is going the next training session.
 * `repet`: I will tell you who is going to the next training session
+* `mail répèt`: I will write a good mail for you (no details here. CLASSIFIED ;)
+* `mail résa`: I will write a good mail for you (no details here. CLASSIFIED ;)
 
 ### Setting it up
 
--- dudunobre/plugins/hello.py
+++ dudunobre/plugins/hello.py
@@ -7,55 +7,39 @@ from slackbot.bot import listen_to
 
 def template_mail_secu(date, place, participants):
     (placename, additionnal_message) = slackbot_settings.PLACES[place]
-    all_participants = map(lambda p: slackbot_settings.PARTICIPANTS[p], participants) + slackbot_settings.PARTICIPANTS.values()
+    all_participants = map(lambda p: slackbot_settings.PARTICIPANTS[p.encode('utf-8')], participants) + slackbot_settings.DEFAULT_PARTICIPANTS.values()
     internal_participants = map(lambda i: i['name'], filter(lambda p: p['is_internal_resource'] == True, all_participants))
     external_participants = map(lambda i: i['name'], filter(lambda p: p['is_internal_resource'] == False, all_participants))
 
-    return '''
-Bonjour à tous,
-
-Voici la liste des personnes présentes ce %s à la répétition de la batucada %s :
-
-%s
-Personnes extérieures à Canal+ :
-
-%s
-
-%s
-
-Merci et bonne journée.
-
-Alexandre
-    ''' % (
+    return slackbot_settings.TEMPLATES['mail_secu']() % (
         date.strftime("%A %d %B"),
         placename,
         '\n'.join(internal_participants),
-        '\n'.external_participants,
+        '\n'.join(external_participants),
         additionnal_message
+    )
+
+def template_mail_booking(date):
+    return slackbot_settings.TEMPLATES['mail_booking']() % (
+        date.strftime("%A %d %B")
     )
 
 
 @respond_to('e alegre$', re.IGNORECASE)
 def greeting_message(message):
-    message.reply('''
-E COLORIDA,
-
-FALO DE QUEM ?
-
-DA BATERIA VILA SENA !
-    ''')
+    message.reply(slackbot_settings.TEMPLATES['samba']())
 
 @respond_to('samba$', re.IGNORECASE)
 def samba(message):
     message.reply('https://www.youtube.com/watch?v=LTqnUudZTME')
 
 @respond_to('^qui a rempli le doodle', re.IGNORECASE)
-def samba(message):
+def who_filled_doodle(message):
     message.reply('hmm, je regarde')
     message.reply('\n'.join(doodle.participants()))
 
 @respond_to('^repet', re.IGNORECASE)
-def samba(message):
+def next_training(message):
     message.reply('atta, je regarde')
     (found_index, date, participants) = doodle.next_training()
     if found_index >= 0:
@@ -65,12 +49,22 @@ def samba(message):
     else:
         message.reply('pas de répèt prévu prochainement')
 
-@respond_to('^mail répèt (.*)', re.IGNORECASE)
-def samba(message, place):
+@respond_to('^mail r.p.t (.*)', re.IGNORECASE)
+def mail_secu(message, place):
     message.reply('2 sec, je check le doodle')
     (found_index, date, participants) = doodle.next_training()
     if found_index >= 0:
-        message.reply(template_mail_secu(date, place, participants))
+        message.reply(template_mail_secu(date, place.strip(), participants))
+    else:
+        message.reply('euh, t\'es sûr qu\'il y a répèt cette semaine-là ?')
+
+
+@respond_to('^mail r.s[a(ervation)]', re.IGNORECASE)
+def mail_booking(message):
+    message.reply('2 sec, je check le doodle')
+    (found_index, date, participants) = doodle.next_training()
+    if found_index >= 0:
+        message.reply(template_mail_booking(date))
     else:
         message.reply('euh, t\'es sûr qu\'il y a répèt cette semaine-là ?')
 
